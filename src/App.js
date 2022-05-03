e5b765d5738bcbd11e20cba599d3fd1ce2dfb5e1
manhtuan112 <manhtuan1122001@gmail.com>
2022-05-03 18:07:16 +0700

    b3 fix tạm thời link in github has bookingFilm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.scss';
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import { routeList } from './config/routeList';
 import Header from './components/header';
 import Footer from './components/footer';
@@ -13,6 +13,7 @@ function App() {
     <>
       <Header />
       <Routes>
+        <Route path='/bookingFilm' element={<Navigate to={routeList.HOME} />}/>
         <Route path={routeList.HOME} element={<Home />} />
         <Route path={routeList.DETAIL} element={<Detal />} />
         <Route path={routeList.CATEGORY} element={<Catalog />} />
