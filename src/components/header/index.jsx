1281bae53a51f6b8ef9e33e9c7dd033d8906004e
manhtuan112 <manhtuan1122001@gmail.com>
2022-05-03 17:50:25 +0700

    b3 build button and modal component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../../assets/tmovie.png";
 import { routeList } from "./../../config/routeList";
@@ -24,24 +24,43 @@ const Header = () => {
   // console.log(pathname)
   const headerRef = useRef(null);
 
+
+  //hieu ung co phan header khi scroll
+  useEffect(() => {
+    const shrinkHeader = () =>{
+      if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+        headerRef.current.classList.add('shrink')
+      }
+      else{
+        headerRef.current.classList.remove('shrink')
+      }
+    }
+
+    window.addEventListener('scroll', shrinkHeader)
+    return () => {
+      window.removeEventListener('scroll', shrinkHeader)
+    };
+  }, []);
+
+
   const active = headerNav.findIndex((e) => e.path === pathname);
 
   return (
     <div ref={headerRef} className="header">
-      <div className="header_wrap container">
+      <div className="header__wrap container">
         <div className="logo">
           <img src={Logo} alt="" />
           <Link to={routeList.HOME}> tMovie </Link>
-        </div>{" "}
+        </div>
         <ul className="header__nav">
-          {" "}
+          
           {headerNav.map((element, index) => (
             <li key={index} className={index === active ? "active" : ""}>
-              <Link to={element.path}> {element.display} </Link>{" "}
+              <Link to={element.path}> {element.display} </Link>
             </li>
-          ))}{" "}
-        </ul>{" "}
-      </div>{" "}
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
---
+++ src/components/modal/index.jsx
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import './modal.scss'
+import { useRef } from 'react';
+
+const Modal = ({ actives, id, children }) => {
+    const [active, setActive] = useState(false);
+    
+    useEffect(() => {
+        setActive(actives)
+    }, [actives]);
+    
+    return (
+        <div id={id} className={`modal ${active ? 'active' : ''}`}>
+            {children}
+        </div>
+    );
+};
+
+
+
+
+Modal.propTypes = {
+    active: PropTypes.bool,
+    id: PropTypes.string
+};
+
+
+export const ModalContent = (props) =>{
+    const contentRef = useRef(null)
+
+    const closeModal = () =>{
+        contentRef.current.parentNode.classList.remove('active')
+        if (props.onClose) props.onClose() 
+    }
+
+    return(
+        <div className="modal__content">
+            {props.children}
+            <div className="modal__content-close" onClick={closeModal}>
+                <i className="bx bx-x"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
