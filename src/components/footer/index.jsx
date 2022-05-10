112ffa4c46cc7636311adbdb7b593ca0a703d63d
manhtuan112 <manhtuan1122001@gmail.com>
2022-05-10 21:00:54 +0700

    b7 make footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import './footer.scss'
+import FooterBG from '../../assets/footer-bg.jpg'
+import logo from '../../assets/tmovie.png'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     
     return (
-        <div>
-            Footer
-            <button onClick={()=> console.log('test')}>
-              Watch now
-            </button>
+        <div className='footer' style={{backgroundImage: `url(${FooterBG})`}}>
+            <div className="footer__content container">
+                <div className="footer__content-logo">
+                    <div className="logo">
+                        <img src={logo} alt="" />
+                        <Link to='/'>Tmovie</Link>
+                    </div>
+                </div>
+                <div className="footer__content-menus">
+                    <div className="footer__content-menu">
+                        <Link to='/'>Home</Link>
+                        <Link to='/'>Contact us</Link>
+                        <Link to='/'>Term of services</Link>
+                        <Link to='/'>About us</Link>
+                    </div>
+                    <div className="footer__content-menu">
+                        <Link to='/'>Live</Link>
+                        <Link to='/'>Contact us</Link>
+                        <Link to='/'>Term of services</Link>
+                        <Link to='/'>About us</Link>
+                    </div>
+                    <div className="footer__content-menu">
+                        <Link to='/'>Home</Link>
+                        <Link to='/'>Contact us</Link>
+                        <Link to='/'>Term of services</Link>
+                        <Link to='/'>About us</Link>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 }
